3ffd1cbfffff1990a9ff7eabc149b622d3ebf0a1
Gerard <gerard.sans@gmail.com>
2014-03-25 20:56:19 +0000

    Added AppContext

---
+++ Company.App.Infraestructure/Company.App.Infraestructure/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Company.App.Domain;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Company.App.Infraestructure
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("AppContext") { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
