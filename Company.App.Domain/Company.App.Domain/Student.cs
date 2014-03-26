fc1ffb35693b0d4fb2eebc722f2e4e4d414a0e0c
Gerard <gerard.sans@gmail.com>
2014-03-26 12:11:43 +0000

    Added annotations for validation and added migration

--- Company.App.Domain/Company.App.Domain/Student.cs
+++ Company.App.Domain/Company.App.Domain/Student.cs
@@ -4,14 +4,21 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;    
+
 namespace Company.App.Domain
 {
     public class Student
     {
         public int ID { get; set; }
+        [DisplayName("Last Name"), StringLength(50, MinimumLength = 2)]
         public string LastName { get; set; }
+        [DisplayName("First and Middle Name"), StringLength(50, MinimumLength = 2)]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date), DisplayName("Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+        [DisplayName("Email")]
         public string Email { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
